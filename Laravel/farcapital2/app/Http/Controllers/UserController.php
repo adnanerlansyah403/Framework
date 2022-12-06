76d52f4ff579b07d6b9ee6eb975675df5f6d9169
adnanerlansyah403 <adnanerlansyah403@gmail.com>
2022-12-07 03:02:23 +0700

    crud user

---
+++ Laravel/farcapital2/app/Http/Controllers/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::query()
+            ->limit(5)
+            ->offset(0)
+            ->get();
+
+        return view("users.list", compact('users'));
+    }
+    public function detail($id)
+    {
+        $user = User::query()
+                    ->where('id', $id)
+                    ->first();
+
+        return view("users.detail", compact('user'));
+    }
+    public function store(Request $request)
+    {
+        $payload = [
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'role' => $request->input('role'),
+        ]  
+
+        $user = User::query()->create($payload);
+        return redirect()->back();
+    } // menambahkan data
+    public function create()
+    {
+        return view('users.create');
+    } // menampilkan halaman membuat data
+    public function update(Request $request, $id = null)
+    {
+        $user = User::query()->where('id', $id)->first();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->role = $request->input('role');
+        $user->save();
+        return redirect()->back();
+    } // mengupdate data
+    public function destroy ($id)
+    {
+        $user = User::query()->where('id', $id)->first();
+        $user->delete();
+        return redirect()->back();
+    }
+}
